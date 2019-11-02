ac818f58a01f4b7de558fefb3938d07b840d8446
cmz1023 <cmz1023@gmail.com>
2019-11-02 08:17:13 -0400

    updated splitByString method

--- qolf.cpp
+++ qolf.cpp
@@ -20,8 +20,8 @@
 std::vector<std::string> qolf::splitString(std::string s1){
     std::vector<std::string> newVec;
     for (int i = 0; i < s1.length(); i++){
-        if (s1.at(i) == ' '){
-            newVec.push_back(s1.substr(0,i));
+        if (s1.at(i) == ' ' || i == s1.length()-1){
+            newVec.push_back(s1.substr(0,i+1));
             s1 = s1.substr(i,s1.length());
             i = 0;
         }
